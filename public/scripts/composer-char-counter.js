03c5fdcab08bed06e6a32027b7557cf4b1e9303f
Roderick Shoolbraid <rdxenv@gmail.com>
2023-11-24 13:56:43 -0800

    counter works :)

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -8,11 +8,14 @@ $(document).ready(function() {
 jQuery(document).ready(function() {
   // Event listener for character input.
   $('.new-tweet textarea').on('input', function() {
+    console.log("text input");
     const textLength = $(this).val().length;
     const remaining = 140 - textLength;
 
     // Assuming .counter is within the same parent element as the textarea
-    const counter = $(this).siblings('.counter');
+    //const counter = $(this).siblings('.counter');
+    const counter = $(".counter");
+    console.log("counter",counter);
     counter.text(remaining);
 
     // Add or remove invalid class based on character count

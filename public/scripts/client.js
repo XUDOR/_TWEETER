c2294aa5c59a03da706de068db4ed786b1b7aa40
Roderick Shoolbraid <rdxenv@gmail.com>
2023-11-27 11:26:22 -0800

    tweet live update client side

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -12,32 +12,39 @@ $(document).ready(function() {
   };
 
   const createTweetElement = function(tweet) {
-    console.log("create");
-    let $tweet = $('<article>').addClass('tweet');
-    $tweet.html(`
-    <header class="tweet-header">
-      <div class="picture">
-        <img src="${tweet.user.avatars}">
-      </div>
-      <div class="name">${tweet.user.name}</div>
-      <div class="user">${tweet.user.handle}</div>  
-    </header>
-    <section class="old-tweet">${tweet.content.text}</section>
-    <footer>
-      <div class="tweet-date">${new Date(tweet.created_at).toLocaleString()}</div>
-      <div class="icons">
-        <i class="fa-solid fa-flag"></i>
-        <i class="fa-solid fa-retweet"></i>
-        <i class="fa-solid fa-heart"></i>
-      </div>
-    </footer>
-  `);
-
+    const avatarSrc = tweet.user.avatars || 'default-avatar-path'; // Update with a valid default path
+    const userName = tweet.user.name || 'Unknown User';
+    const userHandle = tweet.user.handle || '@unknown';
+    const tweetText = tweet.content.text || 'No text provided';
+    const createdAt = new Date(tweet.created_at).toLocaleString();
+  
+    let $tweet = $(`
+      <article class="tweet">
+        <header class="tweet-header">
+          <div class="picture">
+            <img src="${avatarSrc}">
+          </div>
+          <div class="name">${userName}</div>
+          <div class="user">${userHandle}</div>  
+        </header>
+        <section class="old-tweet">${tweetText}</section>
+        <footer>
+          <div class="tweet-date">${createdAt}</div>
+          <div class="icons">
+            <i class="fa-solid fa-flag"></i>
+            <i class="fa-solid fa-retweet"></i>
+            <i class="fa-solid fa-heart"></i>
+          </div>
+        </footer>
+      </article>
+    `);
+  
     const timeagoDate = timeago.format(new Date(tweet.created_at));
     $tweet.find('.tweet-date').text(timeagoDate);
-
+    
     return $tweet;
   };
+
   $('#tweet-form').submit(function(event) {
     event.preventDefault();
 
@@ -56,7 +63,11 @@ $(document).ready(function() {
     console.log('form being sent', serializedData);
 
     $.post('/tweets', serializedData, function(response) {
-      console.log('tweet submitted:', response);   
+      console.log('tweet submitted:', response);
+      console.log('response:', response);
+      const newTweetElement = createTweetElement(response);
+      $('.tweet-list').prepend(newTweetElement);
+      $('#tweet-text').val('');
     }).fail(function(error) {
       console.error('Error submitting tweet:', error);
     });
